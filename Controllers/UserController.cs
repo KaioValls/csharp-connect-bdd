b1f050899f591da3fc3536a505a13fd6b6324b35
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-19 22:04:40 -0300

    First commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using web_app_cache_performance.Model;
+
+namespace web_app_cache_performance.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> getUser()
+        {
+            string connectionString = "Server=localhost;Database=sys;User=root;Password=123;";
+            using var connection = new MySqlConnection(connectionString);
+            await connection.OpenAsync();
+            string query = "select id, nome, email from usuarios;";
+            var usuarios = await connection.QueryAsync<Usuario>(query);
+
+
+            return Ok(usuarios);
+        }
+
+    }
+}
